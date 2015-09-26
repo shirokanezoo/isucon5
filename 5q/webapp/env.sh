b96852152aec303bbd61181400b131d8cb8faab0
shirokanezoo <shirokanezoo@sorah.jp>
2015-09-26 16:33:12 +0900

    Change env.sh

--- 5q/webapp/env.sh
+++ 5q/webapp/env.sh
@@ -1,4 +1,5 @@
 PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
 GOPATH=/home/isucon/gocode
 RACK_ENV=production
-ISUCON5_DISABLE_LOGS=1
+ISUCON5_DB_HOST=127.0.0.1
+ISUCON5_DISABLE_LOGS=0
