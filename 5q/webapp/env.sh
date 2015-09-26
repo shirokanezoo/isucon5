069a8ad3b7cd310c6d130faff9a5e62d5437df96
shirokanezoo <shirokanezoo@sorah.jp>
2015-09-26 15:34:05 +0900

    Update env.sh

--- 5q/webapp/env.sh
+++ 5q/webapp/env.sh
@@ -1,3 +1,4 @@
 PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
 GOPATH=/home/isucon/gocode
 RACK_ENV=production
+ISUCON5_DISABLE_LOGS=1
