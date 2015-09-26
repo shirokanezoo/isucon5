b0279231c58865a6ede883e22511c3025ac149e8
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-09-26 12:27:23 +0900

    unicorn listens UNIX domain socket

--- 5q/webapp/ruby/unicorn_config.rb
+++ 5q/webapp/ruby/unicorn_config.rb
@@ -3,6 +3,7 @@ require 'logger'
 worker_processes 4
 preload_app true
 listen 8080
+listen "/tmp/unicorn.sock", :backlog => 2048
 pid "/home/isucon/webapp/ruby/unicorn.pid"
 stderr_path "/tmp/unicorn-err.log"
 stdout_path "/tmp/unicorn-out.log"
