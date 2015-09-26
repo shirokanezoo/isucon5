5c82a53c164673dfdc62a77b2b67ca0210ee6211
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-09-26 13:52:05 +0900

    DRY relations query

--- 5q/webapp/ruby/app.rb
+++ 5q/webapp/ruby/app.rb
@@ -274,15 +274,6 @@ SQL
       break if comments_of_friends.size >= 10
     end
 
-    # TODO:
-    friends_query = 'SELECT * FROM relations WHERE one = ? OR another = ? ORDER BY created_at DESC'
-    friends_map = {}
-    db.xquery(friends_query, current_user[:id], current_user[:id]).each do |rel|
-      key = (rel[:one] == current_user[:id] ? :another : :one)
-      friends_map[rel[key]] ||= rel[:created_at]
-    end
-    friends = friends_map.map{|user_id, created_at| [user_id, created_at]}
-
     query = <<SQL
 SELECT user_id, owner_id, DATE(created_at) AS date, MAX(created_at) AS updated
 FROM footprints
@@ -299,7 +290,7 @@ SQL
       comments_for_me: comments_for_me,
       entries_of_friends: entries_of_friends,
       comments_of_friends: comments_of_friends,
-      friends: friends,
+      friends: current_friends,
       footprints: footprints
     }
     erb :index, locals: locals
