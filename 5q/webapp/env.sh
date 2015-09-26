7d0dc87385c16238810be0a02ede7f8389410622
shirokanezoo <shirokanezoo@sorah.jp>
2015-09-26 17:58:37 +0900

    disable logs

--- 5q/webapp/env.sh
+++ 5q/webapp/env.sh
@@ -2,4 +2,4 @@ PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/h
 GOPATH=/home/isucon/gocode
 RACK_ENV=production
 ISUCON5_DB_HOST=127.0.0.1
-ISUCON5_DISABLE_LOGS=0
+ISUCON5_DISABLE_LOGS=1
