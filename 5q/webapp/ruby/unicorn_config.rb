008807df1d57dd37f1bd5ece4bc17c67f7b485f7
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-09-26 11:45:58 +0900

    update unicorn config

--- 5q/webapp/ruby/unicorn_config.rb
+++ 5q/webapp/ruby/unicorn_config.rb
@@ -1,4 +1,30 @@
+require 'logger'
+
 worker_processes 1
 preload_app true
 listen 8080
 pid "/home/isucon/webapp/ruby/unicorn.pid"
+stderr_path "/tmp/unicorn-err.log"
+stdout_path "/tmp/unicorn-out.log"
+logger Logger.new($stderr).tap{|l| l.formatter = Logger::Formatter.new }
+
+before_fork do |server, worker|
+  #if defined?(ActiveRecord::Base)
+  #  ActiveRecord::Base.connection_handler.connection_pools.map  {|name, pool| pool.connections }.flatten.each { |c| c.disconnect! }
+  #end
+
+  old_pid_path = "#{config[:pid_path]}.oldbin"
+  if File.exists?(old_pid_path) && server.pid != old_pid_path
+    begin
+      Process.kill("QUIT", File.read(old_pid_path).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(Redis) && Redis.current.connected?
+    Redis.current.client.reconnect
+  end
+end
