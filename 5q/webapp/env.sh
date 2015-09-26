56649f80a2d7ffddf4fe63b83674d9b6fe5f98ed
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-09-26 12:38:26 +0900

    RACK_ENV=production

--- 5q/webapp/env.sh
+++ 5q/webapp/env.sh
@@ -1,2 +1,3 @@
 PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
 GOPATH=/home/isucon/gocode
+RACK_ENV=production
