2ddfd24d48cd491efe3bf9cc44389a8d2603aa3b
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-10-31 16:29:26 +0900

    puma: 3 workers, each worker has 12 threads

--- 5f/webapp/ruby/puma_config.rb
+++ 5f/webapp/ruby/puma_config.rb
@@ -3,5 +3,14 @@ daemonize false
 pidfile '/home/isucon/webapp/ruby/puma.pid'
 state_path '/tmp/puma.state'
 stdout_redirect '/tmp/puma-out.log', '/tmp/puma-err.log', true
-threads 0, 32
 bind 'unix:///tmp/puma.sock'
+
+workers 3
+threads 0, 12
+preload_app!
+
+on_worker_boot do
+  if defined?(Redis) && Redis.current.connected?
+    Redis.current.client.reconnect
+  end
+end
