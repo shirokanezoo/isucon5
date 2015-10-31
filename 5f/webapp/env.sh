93313f146209d4bc2ddd313eb4c1fafbe5b4dc50
Issei Naruta <naruta@cookpad.com>
2015-10-31 12:27:54 +0900

    Specify postgres host

--- 5f/webapp/env.sh
+++ 5f/webapp/env.sh
@@ -2,5 +2,7 @@
 export PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
 export GOPATH=/home/isucon/gocode
 export _JAVA_OPTIONS="-Dfile.encoding=UTF-8"
+export ISUCON5_DB_HOST=isu12a
+export ISUCON5_DB_PORT=5432
 
 exec $*
