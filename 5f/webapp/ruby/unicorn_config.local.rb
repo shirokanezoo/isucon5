4bbf0d0e32beb190e8e25abdd2ebce3f9219c815
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-10-31 11:27:56 +0900

    local unicorn config

---
+++ 5f/webapp/ruby/unicorn_config.local.rb
@@ -0,0 +1,28 @@
+
+require 'logger'
+
+worker_processes 4
+preload_app true
+listen 9292
+logger Logger.new($stderr).tap{|l| l.formatter = Logger::Formatter.new }
+
+before_fork do |server, worker|
+  #if defined?(ActiveRecord::Base)
+  #  ActiveRecord::Base.connection_handler.connection_pools.map  {|name, pool| pool.connections }.flatten.each { |c| c.disconnect! }
+  #end
+
+  old_pid_path = "/home/isucon/webapp/ruby/unicorn.pid.oldbin"
+  if File.exists?(old_pid_path) && server.pid != old_pid_path
+    begin
+      Process.kill("QUIT", File.read(old_pid_path).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(Redis) && Redis.current.connected?
+    Redis.current.client.reconnect
+  end
+end
